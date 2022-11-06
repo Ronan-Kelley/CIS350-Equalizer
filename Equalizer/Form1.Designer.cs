c3482c76fc5c1471ef27bfe92d01388782551792
Ronan Kelley <44382880+Ronan-Kelley@users.noreply.github.com>
2022-11-06 17:54:56 -0500

    implement file picker

--- Equalizer/Form1.Designer.cs
+++ Equalizer/Form1.Designer.cs
@@ -28,12 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this._txt_fileName = new System.Windows.Forms.TextBox();
+            this._btn_browse = new System.Windows.Forms.Button();
+            this._musicFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // _txt_fileName
+            // 
+            this._txt_fileName.Location = new System.Drawing.Point(11, 12);
+            this._txt_fileName.Name = "_txt_fileName";
+            this._txt_fileName.Size = new System.Drawing.Size(696, 23);
+            this._txt_fileName.TabIndex = 0;
+            // 
+            // _btn_browse
+            // 
+            this._btn_browse.Location = new System.Drawing.Point(713, 12);
+            this._btn_browse.Name = "_btn_browse";
+            this._btn_browse.Size = new System.Drawing.Size(75, 23);
+            this._btn_browse.TabIndex = 1;
+            this._btn_browse.Text = "Browse";
+            this._btn_browse.UseVisualStyleBackColor = true;
+            this._btn_browse.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // _musicFileDialog
+            // 
+            this._musicFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this._btn_browse);
+            this.Controls.Add(this._txt_fileName);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private TextBox _txt_fileName;
+        private Button _btn_browse;
+        private OpenFileDialog _musicFileDialog;
     }
 }
--- Equalizer/Form1.cs
+++ Equalizer/Form1.cs
@@ -2,9 +2,45 @@ namespace Equalizer
 {
     public partial class Form1 : Form
     {
+        // I don't know what file types are actually supported,
+        // but this gives us an easy way to change what files can
+        // be selected later on down the line
+        private string[] _filetypes = { "mp3", "wav", "ogg" };
+
         public Form1()
         {
             InitializeComponent();
+
+            // clear the file dialog filter
+            _musicFileDialog.Filter = "";
+
+            // set up the new filter based on the entries in _filetypes
+            string _filterText = "Audio Files (";
+            string _filterFilter = "";
+            foreach (var filetype in _filetypes)
+            {
+                _filterText += $"*.{filetype},";
+                _filterFilter += $"*.{filetype};";
+            }
+
+            // fix the end of both strings before concatenating them together into one
+            _filterText = _filterText.TrimEnd(',') + ")";
+            _filterFilter = _filterFilter.TrimEnd(';');
+
+            // assign the filter to the file picker dialog
+            _musicFileDialog.Filter  = _filterText + "|" + _filterFilter;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // open the file picker dialog when browse is clicked
+            _musicFileDialog.ShowDialog();
+        }
+
+        private void openFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            // display the selected file's path
+            _txt_fileName.Text = _musicFileDialog.FileName;
         }
     }
 }
