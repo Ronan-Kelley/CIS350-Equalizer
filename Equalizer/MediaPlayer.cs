803ff1a4b71d588d751d57fba6c03278a02a04ff
Keagen Talsma <keegenjt@gmail.com>
2022-12-07 14:02:51 -0500

    DOODOO HEAD RONAN
    Setup the files to actually work in VS. Someone (Ronan) did not create these classes the correct way in VS. Now we can actually create UI components!

--- Equalizer/MediaPlayer.cs
+++ Equalizer/MediaPlayer.cs
@@ -1,113 +1,117 @@
 ﻿using NAudio.Wave;
 
-public class MediaPlayer
+namespace Equalizer
 {
-    // player objects
-    private AudioFileReader? _audioFile;
-    private NonLiveEq? _equalizer;
-    private WaveOutEvent? _outputDevice;
+    internal class MediaPlayer
+    {
+        // player objects
+        private AudioFileReader? _audioFile;
+        private NonLiveEq? _equalizer;
+        private WaveOutEvent? _outputDevice;
 
-    // path of the currently loaded file
-    private string? _curFilePath;
+        // path of the currently loaded file
+        private string? _curFilePath;
 
-    public MediaPlayer() {
-        _audioFile = null;
-        _equalizer = null;
-        _outputDevice = null;
-    }
+        public MediaPlayer() {
+            _audioFile = null;
+            _equalizer = null;
+            _outputDevice = null;
+        }
 
-    public bool LoadFile(string filePath) {
-        // ensure file isn't already loaded
-        if (filePath == _curFilePath) {
+        public bool LoadFile(string filePath) {
+            // ensure file isn't already loaded
+            if (filePath == _curFilePath) {
+                return true;
+            }
+
+            // initialize the output device
+            if (_outputDevice == null) {
+                // create the waveout event
+                _outputDevice = new WaveOutEvent();
+                // set up a handler for the PlaybackStopped event (defined below)
+                _outputDevice.PlaybackStopped += OnPlaybackStopped;
+                _outputDevice.Volume = 1;
+            }
+            // initialize the audio file if the path exists
+            if (File.Exists(filePath)) {
+                _audioFile = new AudioFileReader(filePath);
+                _curFilePath = filePath;
+                if (_equalizer == null) {
+                    _equalizer = new NonLiveEq(_audioFile);
+                } else {
+                    _equalizer.SetSource(_audioFile);
+                }
+                _outputDevice.Init(_equalizer);
+            } else {
+                Stop();
+                return false;
+            }
+
             return true;
         }
 
-        // initialize the output device
-        if (_outputDevice == null) {
-            // create the waveout event
-            _outputDevice = new WaveOutEvent();
-            // set up a handler for the PlaybackStopped event (defined below)
-            _outputDevice.PlaybackStopped += OnPlaybackStopped;
-            _outputDevice.Volume = 1;
-        }
-        // initialize the audio file if the path exists
-        if (File.Exists(filePath)) {
-            _audioFile = new AudioFileReader(filePath);
-            _curFilePath = filePath;
-            if (_equalizer == null) {
-                _equalizer = new NonLiveEq(_audioFile);
-            } else {
-                _equalizer.SetSource(_audioFile);
+        public void Play() {
+            if (_outputDevice != null) {
+                // if song is over, restart it
+                if (_audioFile != null && _audioFile.Position >= _audioFile.Length) {
+                    _audioFile.Position = 0;
+                }
+                _outputDevice.Play();
             }
-            _outputDevice.Init(_equalizer);
-        } else {
-            Stop();
-            return false;
         }
 
-        return true;
-    }
-
-    public void Play() {
-        if (_outputDevice != null) {
-            // if song is over, restart it
-            if (_audioFile != null && _audioFile.Position >= _audioFile.Length) {
-                _audioFile.Position = 0;
+        public void Pause() {
+            if (_outputDevice != null) {
+                _outputDevice.Pause();
             }
-            _outputDevice.Play();
-        }
-    }
-
-    public void Pause() {
-        if (_outputDevice != null) {
-            _outputDevice.Pause();
-        }
-    }
-
-    public void Stop() {
-        if (_outputDevice != null) {
-            _outputDevice.Dispose();
-        }
-        _outputDevice = null;
-        if (_audioFile != null) {
-            _audioFile.Dispose();
-        }
-        _audioFile = null;
-        _curFilePath = null;
-    }
-
-    public void setVolumePercentage(float volume) {
-        // clamp passed value
-        if (volume > 100f) {
-            volume = 100f;
-        } else if (volume < 0f) {
-            volume = 0f;
         }
 
-        // set volume
-        if (_outputDevice != null) {
-            _outputDevice.Volume = volume / 100f;
-        }
-    }
-
-    public void setPositionPercent(float percentDone) {
-        // clamp percentDone
-        if (percentDone > 100f) {
-            percentDone = 100f;
-        } else if (percentDone < 0f) {
-            percentDone = 0f;
+        public void Stop() {
+            if (_outputDevice != null) {
+                _outputDevice.Dispose();
+            }
+            _outputDevice = null;
+            if (_audioFile != null) {
+                _audioFile.Dispose();
+            }
+            _audioFile = null;
+            _curFilePath = null;
         }
 
-        // assign the value
-        if (_audioFile != null) {
-            _audioFile.Position = (long)(_audioFile.Length / 100 * (percentDone));
+        public void setVolumePercentage(float volume) {
+            // clamp passed value
+            if (volume > 100f) {
+                volume = 100f;
+            } else if (volume < 0f) {
+                volume = 0f;
+            }
+
+            // set volume
+            if (_outputDevice != null) {
+                _outputDevice.Volume = volume / 100f;
+            }
         }
-    }
 
-    public bool isReady() {
-        return (_outputDevice != null && _audioFile != null);
-    }
+        public void setPositionPercent(float percentDone) {
+            // clamp percentDone
+            if (percentDone > 100f) {
+                percentDone = 100f;
+            } else if (percentDone < 0f) {
+                percentDone = 0f;
+            }
 
-    private void OnPlaybackStopped(object? sender, StoppedEventArgs e) {
+            // assign the value
+            if (_audioFile != null) {
+                _audioFile.Position = (long)(_audioFile.Length / 100 * (percentDone));
+            }
+        }
+
+        public bool isReady() {
+            return (_outputDevice != null && _audioFile != null);
+        }
+
+        private void OnPlaybackStopped(object? sender, StoppedEventArgs e) {
+        }
     }
 }
+
--- Equalizer/NonLiveEq.cs
+++ Equalizer/NonLiveEq.cs
@@ -6,124 +6,118 @@ using System;
 using NAudio.MediaFoundation;
 using System.Security.Cryptography.X509Certificates;
 
+namespace Equalizer
+{
+    internal class NonLiveEq : ISampleProvider
+    {
+        private ISampleProvider _sourceProvider;
+
+        // Filter variables
+        private BiQuadFilter[] _filters;
+        private bool _enabled;
+        private int _numOfFilters;
+
+        // TODO this could throw error if _sourceProvider is null
+        WaveFormat ISampleProvider.WaveFormat => _sourceProvider.WaveFormat;
+
+        public NonLiveEq(ISampleProvider sourceProvider) {
+            // TODO could throw error if null
+            _sourceProvider = sourceProvider;
+
+            // Equalizer variables setup 
+            _filters = new BiQuadFilter[10];
+            _enabled = false;
+            _numOfFilters = 0;
+
+            // For testing EQ
+            AddFilter(1000, 0.5f, 10);
+            EnableFilter();
+        }
+
+        public void SetSource(ISampleProvider newSourceProvider) {
+            if (newSourceProvider == null) {
+                return;
+            }
+
+            _sourceProvider = newSourceProvider;
+        }
+
+        public int Read(float[] buffer, int offset, int count) {
+            int samplesRead = _sourceProvider.Read(buffer, offset, count);
+
+            if (!_enabled) {
+                return samplesRead;
+            }
+
+            for (int i = 0; i < samplesRead; i++) {
+                for (int filterIndex = 0; filterIndex < _numOfFilters; filterIndex++) {
+                    buffer[offset + i] = _filters[filterIndex].Transform(buffer[offset + i]);
+                }
+            }
+
+            return samplesRead;
+        }
+
+        /// <summary>
+        /// Sets one band of audio equalization at the given params. 
+        /// </summary>
+        /// <param name="centerFreq">
+        /// Center frequency for the filter
+        /// </param>
+        /// <param name="q">
+        /// The range around the center frequency. (A high <c>q</c>
+        /// value only affects freqencies very close to it).
+        /// </param>
+        /// <param name="dbGain">
+        /// The maximum decibal gain at the center frequency 
+        /// </param>
+        public void SetFilter(int index, float centerFreq, float q, float dbGain) {
+            if (index < 0 || index >= _numOfFilters) {
+                return; //TODO error handeling?
+            }
+            _filters[index].SetPeakingEq(_sourceProvider.WaveFormat.SampleRate, centerFreq, q, dbGain);
+        }
+
+        public void AddFilter(float centerFreq, float q, float dbGain) {
+            if (_numOfFilters == 10) {
+                return;
+            }
+            _filters[_numOfFilters] = BiQuadFilter.PeakingEQ(_sourceProvider.WaveFormat.SampleRate, centerFreq, q, dbGain);
+            _numOfFilters++;
+        }
+
+        public void RemoveFilter(int index) {
+            if (index < 0 || index >= _numOfFilters) {
+                return; //TODO error handeling?
+            }
+
+            // Shift all other filters in array
+            while (index < _numOfFilters - 1) {
+                _filters[index] = _filters[index + 1];
+                index++;
+            }
+
+            _numOfFilters--;
+        }
+
+        /// <summary>
+        /// Enables the captured audio to be altered based on filter
+        /// before being played back.
+        /// </summary>
+        public void EnableFilter() {
+            _enabled = true;
+        }
+
+        /// <summary>
+        /// Stops the captured audio from being altered before
+        /// being played back.
+        /// </summary>
+        public void DisableFilter() {
+            _enabled = false;
+        }
+    }
+}
 /// <summary>
 /// Captures, alters, and plays back system audio.
 /// </summary>
-public class NonLiveEq : ISampleProvider
-{
-	private ISampleProvider _sourceProvider;
 
-    // Filter variables
-    private BiQuadFilter[] _filters;
-    private bool _enabled;
-    private int _numOfFilters;
-
-	// TODO this could throw error if _sourceProvider is null
-	WaveFormat ISampleProvider.WaveFormat => _sourceProvider.WaveFormat;
-
-	public NonLiveEq(ISampleProvider sourceProvider) {
-		// TODO could throw error if null
-		_sourceProvider = sourceProvider;
-
-		// Equalizer variables setup 
-		_filters = new BiQuadFilter[10];
-		_enabled = false;
-		_numOfFilters = 0;
-
-		// For testing EQ
-		AddFilter(1000, 0.5f, 10);
-		EnableFilter();
-	}
-
-	public void SetSource(ISampleProvider newSourceProvider) {
-		if (newSourceProvider == null) {
-			return;
-		}
-
-		_sourceProvider = newSourceProvider;
-	}
-
-	public int Read(float[] buffer, int offset, int count) {
-		int samplesRead = _sourceProvider.Read(buffer, offset, count);
-
-		if (!_enabled)
-		{
-			return samplesRead;
-		}
-
-		for (int i = 0; i < samplesRead; i++)
-		{
-			for (int filterIndex = 0; filterIndex < _numOfFilters; filterIndex++)
-			{
-				buffer[offset + i] = _filters[filterIndex].Transform(buffer[offset + i]);
-			}
-		}
-
-		return samplesRead;
-	}
-
-	/// <summary>
-	/// Sets one band of audio equalization at the given params. 
-	/// </summary>
-	/// <param name="centerFreq">
-	/// Center frequency for the filter
-	/// </param>
-	/// <param name="q">
-	/// The range around the center frequency. (A high <c>q</c>
-	/// value only affects freqencies very close to it).
-	/// </param>
-	/// <param name="dbGain">
-	/// The maximum decibal gain at the center frequency 
-	/// </param>
-	public void SetFilter(int index, float centerFreq, float q, float dbGain)
-	{
-		if (index < 0 || index >= _numOfFilters)
-		{
-			return; //TODO error handeling?
-		}
-		_filters[index].SetPeakingEq(_sourceProvider.WaveFormat.SampleRate, centerFreq, q, dbGain);
-	}
-
-	public void AddFilter(float centerFreq, float q, float dbGain)
-	{
-		if (_numOfFilters == 10)
-		{
-			return;
-		}
-		_filters[_numOfFilters] = BiQuadFilter.PeakingEQ(_sourceProvider.WaveFormat.SampleRate, centerFreq, q, dbGain);
-		_numOfFilters++;
-	}
-
-    public void RemoveFilter(int index)
-	{
-		if (index < 0 || index >= _numOfFilters)
-		{
-			return; //TODO error handeling?
-		}
-
-		// Shift all other filters in array
-		while (index < _numOfFilters - 1)
-		{
-			_filters[index] = _filters[index + 1];
-			index++;
-		}
-
-		_numOfFilters--;
-	}
-
-    /// <summary>
-    /// Enables the captured audio to be altered based on filter
-    /// before being played back.
-    /// </summary>
-    public void EnableFilter() {
-        _enabled = true;
-    }
-
-    /// <summary>
-    /// Stops the captured audio from being altered before
-    /// being played back.
-    /// </summary>
-    public void DisableFilter() {
-        _enabled = false;
-    }
-}
